cbf5a78af2b879ddeca4f625ef1d3aacc5758fc9
Daniel Olson <emaildano@gmail.com>
2021-03-24 16:39:03 -0400

    WIP

--- classmap.php
+++ classmap.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * WP Search With Algolia "Classmap" file.
  *
@@ -8,48 +9,7 @@
  * @package DigitalCube\Galaxy
  */
 
-if ( ! defined( 'ALGOLIA_PATH' ) ) {
-	exit();
-}
-
-// The Algolia Search PHP API SearchClient.
-if ( apply_filters( 'algolia_should_require_search_client', true ) ) {
-	require_once ALGOLIA_PATH . 'includes/libraries/algoliasearch-client-php/autoload.php';
-}
-
-require_once ALGOLIA_PATH . 'includes/factories/class-algolia-http-client-interface-factory.php';
-require_once ALGOLIA_PATH . 'includes/factories/class-algolia-search-client-factory.php';
-require_once ALGOLIA_PATH . 'includes/factories/class-algolia-plugin-factory.php';
-
-require_once ALGOLIA_PATH . 'includes/class-algolia-api.php';
-require_once ALGOLIA_PATH . 'includes/class-algolia-autocomplete-config.php';
-require_once ALGOLIA_PATH . 'includes/class-algolia-compatibility.php';
-require_once ALGOLIA_PATH . 'includes/class-algolia-plugin.php';
-require_once ALGOLIA_PATH . 'includes/class-algolia-search.php';
-require_once ALGOLIA_PATH . 'includes/class-algolia-settings.php';
-require_once ALGOLIA_PATH . 'includes/class-algolia-template-loader.php';
-require_once ALGOLIA_PATH . 'includes/class-algolia-utils.php';
-require_once ALGOLIA_PATH . 'includes/class-algolia-styles.php';
-require_once ALGOLIA_PATH . 'includes/class-algolia-scripts.php';
-
-require_once ALGOLIA_PATH . 'includes/indices/class-algolia-index.php';
-require_once ALGOLIA_PATH . 'includes/indices/class-algolia-index-replica.php';
-require_once ALGOLIA_PATH . 'includes/indices/class-algolia-searchable-posts-index.php';
-require_once ALGOLIA_PATH . 'includes/indices/class-algolia-posts-index.php';
-require_once ALGOLIA_PATH . 'includes/indices/class-algolia-terms-index.php';
-require_once ALGOLIA_PATH . 'includes/indices/class-algolia-users-index.php';
-
-require_once ALGOLIA_PATH . 'includes/watchers/class-algolia-changes-watcher.php';
-require_once ALGOLIA_PATH . 'includes/watchers/class-algolia-post-changes-watcher.php';
-require_once ALGOLIA_PATH . 'includes/watchers/class-algolia-term-changes-watcher.php';
-require_once ALGOLIA_PATH . 'includes/watchers/class-algolia-user-changes-watcher.php';
-
+require_once ALGOLIA_PATH . 'includes/factories/class-plugin-factory.php';
+require_once ALGOLIA_PATH . 'includes/algolia/algolia.php';
 require_once ALGOLIA_PATH . 'includes/elementor/class-algolia-index.php';
 require_once ALGOLIA_PATH . 'includes/blocks/index.php';
-
-if ( is_admin() ) {
-	require_once ALGOLIA_PATH . 'includes/admin/class-algolia-admin.php';
-	require_once ALGOLIA_PATH . 'includes/admin/class-algolia-admin-page-autocomplete.php';
-	require_once ALGOLIA_PATH . 'includes/admin/class-algolia-admin-page-native-search.php';
-	require_once ALGOLIA_PATH . 'includes/admin/class-algolia-admin-page-settings.php';
-}
